0b1d22970d36ecb2be6549045b1ee27a4180feaa
manishdusari <manishrajgouddusari@gmail.com>
2020-04-14 09:48:22 +0530

    Word Count Class Implementation

--- Assignment3/Multithreads/src/WordCount.java
+++ Assignment3/Multithreads/src/WordCount.java
@@ -8,14 +8,27 @@ import java.util.Map;
  */
 public class WordCount {
 
-	        public String[] filteredList( String[] words ) {
-				return null;
-	        }
+	public String[] filteredList(String[] words) {
+		List<String> filteredList = new ArrayList<>();
+		for (String word : words) {
+			if (word.matches("[a-zA-Z]+")) {
+				filteredList.add(word);
+			}
+		}
+		return filteredList.toArray(new String[filteredList.size()]);
+	}
 
-
-	        public synchronized void fillingWordCounter( Map<String, Integer> wordCounts, String word, int count )
-	        {
-	            
-	        }
+	public synchronized void fillingWordCounter(Map<String, Integer> wordCounts, String word, int count) {
+		if (wordCounts.containsKey(word)) {
+			wordCounts.put(word, wordCounts.get(word) + 1);
+		} else {
+			wordCounts.put(word, 1);
+		}
+		if (count == 0) {
+			System.out.printf("Current Thread Running: %s\n", Thread.currentThread().getName());
+			System.out.printf("%-9s %13s %n", "WORD", "COUNT");
+		}
+		System.out.printf("%-15s", word);
+		System.out.printf("%5d\n", wordCounts.get(word));
+	}
 }
-
