3d4592cc97bc1a3b51bce4f7cd860b503b7a1c64
vishwasuvarna07 <vishwatejasuvarna@gmail.com>
2020-04-13 20:29:18 -0500

    Corrected Word count class

--- Assignment3/Multithreads/src/WordCount.java
+++ Assignment3/Multithreads/src/WordCount.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -11,44 +12,6 @@ import java.util.concurrent.TimeUnit;
 public class WordCount
 {
     private static final int THREAD_COUNT = 10;
-
-
-    private static class FileProcessing implements Iterator<Object>, AutoCloseable
-    {
-        private final BufferedReader buffReader;
-        private String nextLine;
-
-
-        public FileProcessing( String fileName ) throws IOException
-        {
-        	buffReader = new BufferedReader( new FileReader( fileName ) );
-            nextLine = buffReader.readLine();
-        }
-
-
-        @Override public boolean hasNext()
-        {
-            return nextLine != null;
-        }
-
-
-        @Override public String next()
-        {
-            String lineToReturn = nextLine;
-            try {
-                nextLine = buffReader.readLine();
-            } catch ( IOException e ) {
-                nextLine = null;
-            }
-            return lineToReturn;
-        }
-
-
-        @Override public void close() throws IOException
-        {
-        	buffReader.close();
-        }
-    }
     
 
     public static void main( final String[] args ) throws Exception
@@ -61,25 +24,42 @@ public class WordCount
         {
             @Override public void run()
             {
-                try ( FileProcessing fc = new FileProcessing( input ) ) {
-                    while ( fc.hasNext() ) {
-                        dataQueue.add( fc.next() );
-                    }
-                } catch ( IOException e ) {
-                    e.printStackTrace();
-                }
+            	BufferedReader buffReader;
+				try {
+					buffReader = new BufferedReader( new FileReader( input ) );
+					String nextLine =  buffReader.readLine();
+					while(nextLine != null){
+						String line = nextLine;
+			            try {
+			                nextLine = buffReader.readLine();
+			            } catch ( IOException e ) {
+			                nextLine = null;
+			            }
+			            dataQueue.add( line );
+						
+					}	
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
             }
         }.start();
         while ( dataQueue.isEmpty() ) {
             // Wait for the thread to start writing into the queue
             Thread.sleep( 100 );
         }
-        ExecutorService es = Executors.newFixedThreadPool( THREAD_COUNT );
-        for ( int i = 0; i < THREAD_COUNT; i++ ) {
-//            es.execute( new TransformationThread( tr, wordCounts, dataQueue ) );
+        ExecutorService executorService = Executors.newFixedThreadPool( THREAD_COUNT );
+        int i = 0;
+        while(i < THREAD_COUNT) {
+//            executorService.execute( new TransformationThread( tr, wordCounts, dataQueue ) );
+        	i++;
         }
-        es.shutdown();
-        es.awaitTermination( 1, TimeUnit.MINUTES );
+        executorService.shutdown();
+        executorService.awaitTermination( 1, TimeUnit.MINUTES );
         System.out.println( "Total number of words:\n" + wordCounts );
     }
 }
